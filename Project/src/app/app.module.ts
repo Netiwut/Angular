72b6628bb9e19515031c2e931c349129f698597a
Your Name <netiwut.10@gmail.com>
2018-05-06 17:14:05 +0700

    create index and create page

--- Project/src/app/app.module.ts
+++ Project/src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { IndexComponent } from './index/index.component';
+import { CreateComponent } from './create/create.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    CreateComponent
   ],
   imports: [
     BrowserModule
