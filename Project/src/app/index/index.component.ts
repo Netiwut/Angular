53c044fee5b3451960ce0b76e52df07936fccc5c
Your Name <netiwut.10@gmail.com>
2018-05-08 23:09:15 +0700

    add service

--- Project/src/app/index/index.component.ts
+++ Project/src/app/index/index.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import {map} from 'rxjs/operators';
+import {HttpClient} from '@angular/common/http';
 @Component({
   selector: 'app-index',
   templateUrl: './index.component.html',
