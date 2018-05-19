da019cbcb9caa49ad7bf6aaf6b9b91592e6ceb9b
Netiwut <takter_10@hotmail.com>
2018-05-19 23:31:12 +0700

    แอดแล้วน้าา

--- FinalProject/src/app/app.module.ts
+++ FinalProject/src/app/app.module.ts
@@ -3,12 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { SidebarModule } from 'ng-sidebar';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SidebarModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
