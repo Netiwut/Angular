c9fdcd7f747f1a8d63d6fb449d40e2f7e63a443e
Netiwut <takter_10@hotmail.com>
2018-05-23 01:49:15 +0700

    เย้ๆๆๆ ง่วงจัง

--- FinalProject/src/app/components/gallery/photo-all/photo-all.component.ts
+++ FinalProject/src/app/components/gallery/photo-all/photo-all.component.ts
@@ -2,16 +2,30 @@ import { Component, OnInit, Input } from '@angular/core';
 import { BackendApiService } from './../../../services/backend-api.service';
 import { Gallery, General } from './../../../models/models';
 import { ActivatedRoute } from '@angular/router';
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-photo-all',
   templateUrl: './photo-all.component.html',
-  styleUrls: ['./photo-all.component.css']
+  styleUrls: ['./photo-all.component.css'],
+  styles: [`
+    .dark-modal .modal-content {
+      background-color: #292b2c;
+      color: white;
+    }
+    .dark-modal .close {
+      color: white;
+    }
+    .light-blue-backdrop {
+      background-color: #5cb3fd;
+    }
+  `]
 })
 export class PhotoAllComponent implements OnInit {
   id: any;
   gallery: Gallery;
-  constructor(private backendApiService: BackendApiService, private route: ActivatedRoute) {
+  closeResult: string;
+  constructor(private backendApiService: BackendApiService, private route: ActivatedRoute, private modalService: NgbModal) {
     this.route.params.subscribe( params => this.id = params['id'] );
   }
   ngOnInit() {
@@ -22,5 +36,24 @@ export class PhotoAllComponent implements OnInit {
         // this.length = this.gallery.length;
       }
     );
+  }
+  openBackDropCustomClass(content) {
+    this.modalService.open(content, {backdropClass: 'light-blue-backdrop'});
+  }
+
+  openWindowCustomClass(content) {
+    this.modalService.open(content, { windowClass: 'dark-modal' });
+  }
+
+  openSm(content) {
+    this.modalService.open(content, { size: 'sm' });
+  }
+
+  openLg(content) {
+    this.modalService.open(content, { size: 'lg' });
+  }
+
+  openVerticallyCentered(content) {
+    this.modalService.open(content, { centered: true });
   }
 }
