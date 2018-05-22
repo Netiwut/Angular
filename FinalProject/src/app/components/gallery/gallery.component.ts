7f9aaa3bcd6d640a00ee8b617b8bef461936e3f2
Netiwut <takter_10@hotmail.com>
2018-05-22 21:38:06 +0700

    Add All backend

--- FinalProject/src/app/components/gallery/gallery.component.ts
+++ FinalProject/src/app/components/gallery/gallery.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-
+import { BackendApiService } from './../../services/backend-api.service';
+import { Gallery, General } from './../../models/models';
 @Component({
   selector: 'app-gallery',
   templateUrl: './gallery.component.html',
@@ -8,25 +9,15 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 })
 export class GalleryComponent implements OnInit {
-  pics = [
-    {url: 'https://upload.wikimedia.org/wikipedia/commons/e/ee/Mango_and_cross_section_edit.jpg',albumname: "มะม่วง" },
-    {url: 'http://i.dailymail.co.uk/i/newpix/2018/03/08/11/49FF748700000578-5476901-image-a-27_1520507605152.jpg',albumname: "สัปปะรด"},
-    {url: 'https://www.well-beingsecrets.com/wp-content/uploads/Helps-Treat-Duchenne-Muscular-Dystrophy.jpg',albumname: "แตงโม"},
-    {url: 'http://blends.co.in/wp-content/uploads/2015/09/Watermelon.jpg',albumname: "มะม่วง" },
-    {url: 'https://ae01.alicdn.com/kf/HTB1lEMlLpXXXXbcXVXXq6xXFXXX4/Rare-Purple-Flesh-Watermelon-Seeds-Super-Big-Water-Melon-Seeds-for-Home-Garden-10pcs-lot.jpg_640x640.jpg',albumname: "แตงโม"},
-    {url: 'https://img.taste.com.au/VLvRnSwj/taste/2016/11/watermelon-whale-72575-1.jpeg',albumname: "สัปปะรด"},
-    {url: 'https://upload.wikimedia.org/wikipedia/commons/e/ee/Mango_and_cross_section_edit.jpg',albumname: "มะม่วง" },
-    {url: 'http://i.dailymail.co.uk/i/newpix/2018/03/08/11/49FF748700000578-5476901-image-a-27_1520507605152.jpg',albumname: "สัปปะรด"},
-    {url: 'https://www.well-beingsecrets.com/wp-content/uploads/Helps-Treat-Duchenne-Muscular-Dystrophy.jpg',albumname: "แตงโม"},
-    {url: 'http://blends.co.in/wp-content/uploads/2015/09/Watermelon.jpg',albumname: "มะม่วง" },
-    {url: 'https://ae01.alicdn.com/kf/HTB1lEMlLpXXXXbcXVXXq6xXFXXX4/Rare-Purple-Flesh-Watermelon-Seeds-Super-Big-Water-Melon-Seeds-for-Home-Garden-10pcs-lot.jpg_640x640.jpg',albumname: "แตงโม"},
-    {url: 'https://img.taste.com.au/VLvRnSwj/taste/2016/11/watermelon-whale-72575-1.jpeg',albumname: "สัปปะรด"}
-  ];
-
-
-  constructor() { }
+  gallery: Gallery[];
+  constructor(private backendApiService: BackendApiService) { }
 
   ngOnInit() {
+    this.backendApiService.getGallery().subscribe(
+      (data: Gallery[]) => {
+        this.gallery = data;
+      }
+    );
   }
 
 }
--- FinalProject/src/app/components/gallery/photo-all/photo-all.component.ts
+++ FinalProject/src/app/components/gallery/photo-all/photo-all.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { BackendApiService } from './../../../services/backend-api.service';
+import { Gallery, General } from './../../../models/models';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-photo-all',
@@ -6,12 +9,18 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./photo-all.component.css']
 })
 export class PhotoAllComponent implements OnInit {
-
-  constructor() { 
-    
+  id: any;
+  gallery: Gallery;
+  constructor(private backendApiService: BackendApiService, private route: ActivatedRoute) {
+    this.route.params.subscribe( params => this.id = params['id'] );
   }
-
   ngOnInit() {
+    this.backendApiService.getGallery().subscribe(
+      (data: Gallery[]) => {
+        this.gallery = data[this.id];
+        console.log(data[this.id]);
+        // this.length = this.gallery.length;
+      }
+    );
   }
-
 }
