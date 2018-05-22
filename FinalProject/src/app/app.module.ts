013e25bd4e63f813121494572b5ceff2cf57505a
Netiwut <takter_10@hotmail.com>
2018-05-23 02:09:48 +0700

    Takter Add Backend ง่วงจางงงงง งือออ

--- FinalProject/src/app/app.module.ts
+++ FinalProject/src/app/app.module.ts
@@ -43,6 +43,10 @@ const appRoutes = [
     component: IndexComponent
   },
   {
+    path: 'mynavbe',
+    component: MyNavbeComponent
+  },
+  {
     path: 'mynav',
     component: MyNavComponent,
     children: [
